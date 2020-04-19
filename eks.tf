abf0990c6437a170cba593ca6c1f6eb613d47434
Sebastien Pincemail <extern.sebastien.pincemail@europcar.com>
2020-04-19 18:53:01 +0200

    Itinital commit

---
+++ eks.tf
@@ -0,0 +1,43 @@
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = var.eks_cluster_name
+  subnets      = module.vpc.private_subnets
+
+  tags = {
+    Environment = var.env
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  node_groups_defaults = {
+    ami_type  = "AL2_x86_64"
+    disk_size = 50
+  }
+
+  node_groups = {
+    example = {
+      desired_capacity = var.desired-capacity
+      max_capacity     = var.max-size
+      min_capacity     = var.min-size
+
+      instance_type = "m5.large"
+      k8s_labels = {
+        Environment = var.env
+        AppRole  = var.AppRole
+        ManagedBy   = var.ManagedBy
+      }
+      additional_tags = {
+        ExtraTag = "example"
+      }
+    }
+  }
+
+
+
+  map_roles    = var.map_roles
+  map_users    = var.map_users
+  map_accounts = var.map_accounts
+
+}
---
+++ vpc.tf
@@ -0,0 +1,52 @@
+
+
+resource "aws_eip" "nat" {
+  count = 1
+  vpc = true
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.prefix}-gbi-${var.application}-${var.env}-${var.region_trigram}-vpc"
+  cidr = var.vpc_cidr
+
+  azs                 = var.azs
+  public_subnets      = var.public_subnets
+  database_subnets    = var.database_subnets
+  private_subnets     = var.private_subnets
+  external_nat_ip_ids = aws_eip.nat.*.id
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
+  reuse_nat_ips          = true
+  one_nat_gateway_per_az = false
+
+
+  public_subnet_tags = {
+    Name = var.vcp_pub_sub_tags
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+
+  database_subnet_tags = {
+    Name = "${var.eks_cluster_name}-eks-db"
+  }
+
+  tags = {
+    Owner     = var.owner
+    Env       = var.env
+    Project   = var.application
+    AppRole   = var.AppRole
+    ManagedBy = var.ManagedBy
+    Name                                        = "${var.eks_cluster_name}-vpc"
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+  }
+}
