110faaeae94730b01d754a0c14593b38128c3b1c
Sebastien Pincemail <extern.sebastien.pincemail@europcar.com>
2021-02-02 22:06:10 +0100

    add provider version

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  #version                = "~> 1.11.0"
+  version                = "~> 1.11.0"
 }
 
 
